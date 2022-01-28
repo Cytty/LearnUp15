637af10fc83e8a3c24d9781266bec5d164f4f995
Cytty <cytty42@gmail.com>
2022-01-28 21:34:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+class Main {
+    public static void main(String[] args) {
+        boolean isGreenLight = false;
+
+        int speedOfPlayer1 = 5;
+        int speedOfPlayer2 = 0;
+        int speedOfPlayer3 = 2;
+
+        int nOfPlayers = 0;
+
+        if (speedOfPlayer1 > 0) {
+            nOfPlayers = (nOfPlayers + 1);
+        }
+        if (speedOfPlayer2 > 0) {
+            nOfPlayers = (nOfPlayers + 1);
+        }
+        if (speedOfPlayer3 > 0) {
+            nOfPlayers = (nOfPlayers + 1);
+        }
+        if (isGreenLight == false) {
+            System.out.println("Выбыло игроков: " + nOfPlayers);
+        } else {
+            System.out.println("Выбывших нет");
+        }
+
+
+    }
+}
